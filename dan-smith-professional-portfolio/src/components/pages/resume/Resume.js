35a2141ec6a51d6889e3351ee8327aef41a8cf8c
dansmith09 <dansmith_09@hotmail.com>
2022-10-03 18:42:38 +1100

    Added resume download functionality

--- dan-smith-professional-portfolio/src/components/pages/resume/Resume.js
+++ dan-smith-professional-portfolio/src/components/pages/resume/Resume.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import dlResume from '../../../assets/files/Daniel-Smith-Resume-2022.docx'
 
 export default function Resume() {
   return (
     <div>
         <iframe title='resume' src="https://drive.google.com/file/d/1oDHzjStvXmT29Mt4ZINAHHfh_QVHkHsw/preview" allow="autoplay"></iframe>
         <div className='resumeButtonHolder'>
-          <button className='resumeButton'>Download Resume</button>
+          <a class="resume-download"  href={dlResume} download="Resume">
+            <button className='resumeButton'>Download Resume</button>
+          </a>
+        </div>
+        <div>
+          Proficient in:
+          <ul>
+            <li>add proficiencies here</li>
+            <li>add proficiencies here</li>
+            <li>add proficiencies here</li>
+            <li>add proficiencies here</li>
+            <li>add proficiencies here</li>
+            <li>add proficiencies here</li>
+          </ul>
         </div>
     </div>
   )
