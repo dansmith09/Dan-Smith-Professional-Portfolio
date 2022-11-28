ba246fab4c6604e194be570afafefdf26c902adc
dansmith09 <dansmith_09@hotmail.com>
2022-11-28 16:18:32 +1100

    Updated about page

--- dan-smith-professional-portfolio/src/components/pages/about/About.js
+++ dan-smith-professional-portfolio/src/components/pages/about/About.js
@@ -6,8 +6,9 @@ export default function About() {
         <div className='aboutAvatar'></div>
         <p>Hey! My name is Dan.<br></br>
           I'm a 28 year old web developer.<br></br>
-          I'm passionate about building in the crypto/web3 community and became a developer because of this passion.<br></br>
-          It is my goal to get involved with or start a project in the space in the near future.<br></br>
+          I have recently completed a 6 month, intensive full stack developer bootcamp with the University of Sydney and Trilogy Education Services where I achieved the highest grade in project work of my cohort (99.33%).<br></br>
+          I also have a Bachelor of Commerce degree from the University of Adelaide where I majored in Corporate Finance.
+          It is my next goal to secure an exciting and challenging role where I can learn and grow as a developer.<br></br>
           Offline I enjoy practicing yoga, playing guitar, swimming in the ocean, playing golf, drinking coffee and eating meals with my friends.<br></br>
           I also play several musical instruments.<br></br>
           </p>
